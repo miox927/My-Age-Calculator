dfe1b27a7d28f25c64cb8e831a36d0011beb0baa
miox927 <rabha927@gmail.com>
2026-02-21 10:57:26 +0530

    Add service worker for age calculator app
    Implement service worker for caching and fetch handling.

---
+++ sw.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = 'age-calculator-v1';
+const urlsToCache = [
+  '/My-Age-Calculator/',
+  '/My-Age-Calculator/index.html',
+  '/My-Age-Calculator/manifest.json',
+  '/My-Age-Calculator/icon-192.png',
+  '/My-Age-Calculator/icon-512.png'
+];
+
+// Install service worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cache opened');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch requests
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+// Activate and clean old caches
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
